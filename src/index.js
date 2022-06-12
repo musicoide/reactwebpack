95b75d48703251091f1bdf3bcbbd078cc8090ede
musicoide <musicoide0@gmail.com>
2022-06-12 15:45:55 -0300

    subiendo el proyecto react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/global.scss";
+
+
+ReactDOM.createRoot(document.getElementById('App')).render(<App/>)
